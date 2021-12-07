0930f8d67713e76f654c429f1ba9f8e53f8a7930
grudgecat <grudgecat@protonmail.com>
2021-12-07 15:32:33 -0800

    added class code from assignment 6, edited for assignment 8. Added main section.

--- Assigment08.py
+++ Assigment08.py
@@ -5,7 +5,7 @@
 # ChangeLog (Who,When,What):
 # RRoot,1.1.2030,Created started script
 # RRoot,1.1.2030,Added pseudo-code to start assignment 8
-# <Your Name>,<Today's Date>,Modified code to complete assignment 8
+# Sheri Elgin, Dec 6, 2021, Modified code to complete assignment 8
 # ------------------------------------------------------------------------ #
 
 # Data -------------------------------------------------------------------- #
@@ -21,10 +21,39 @@ class Product:
     methods:
     changelog: (When,Who,What)
         RRoot,1.1.2030,Created Class
-        <Your Name>,<Today's Date>,Modified code to complete assignment 8
+        Sheri Elgin, Dec 6, 2021, Modified code to complete assignment 8
     """
-    pass
-    # TODO: Add Code to the Product class
+
+    # --Fields--
+    # strFirstName = ""
+
+    # -- Constructor --
+    def __init__(self, product_name, product_price):
+        # -- Attributes --
+        self.product_name = product_name
+        self.product_price = product_price
+
+    # -- Properties --
+    # NAME
+    @property #getter
+    def product_name(self):
+        return str(self.__product_name).product_name()
+
+    @product_name.setter
+    def product_name(self, value):
+        if str(value).isnumeric() == False:
+            self.__first_name = value
+        else:
+            raise Exception("Product names cannot be numbers")
+
+    # PRICE
+    @property
+    def product_price(self):
+        return str(self.__product_price).product_price()
+
+    @product_price.setter
+    def product_price(self, value):
+        self.__product_price = value
 # Data -------------------------------------------------------------------- #
 
 # Processing  ------------------------------------------------------------- #
@@ -38,90 +67,75 @@ class FileProcessor:
 
     changelog: (When,Who,What)
         RRoot,1.1.2030,Created Class
-        <Your Name>,<Today's Date>,Modified code to complete assignment 8
+        Sheri Elgin, Dec 6, 2021, Modified code to complete assignment 8
     """
    
     @staticmethod
-    def read_data_from_file(file_name, list_of_rows):
-        """ Reads data from a file into a list of dictionary rows
+    def read_data_from_file(file_name, list_of_product_objects):
+        """ Reads data from a file into a list of product objects
 
         :param file_name: (string) with name of file:
-        :param list_of_rows: (list) you want filled with file data:
-        :return: (list) of dictionary rows
+        :param list_of_product_objects: (list) you want filled with file data:
+        :return: (list) of products:
         """
-        list_of_rows.clear()  # clear current data
+        list_of_product_objects.clear()  # clear current data
         file = open(file_name, "r")
         for line in file:
-            task, priority = line.split(",")
-            row = {"Task": task.strip(), "Priority": priority.strip()}
-            list_of_rows.append(row)
+            product, price = line.split(",")
+            objProduct = Product(product, price)
+            list_of_product_objects.append(objProduct)
         file.close()
-        return list_of_rows, 'Success'
+        return list_of_product_objects
 
     @staticmethod
-    def add_data_to_list(task, priority, list_of_rows):
-        """ Adding task and priority as dict to list_of_rows and returning new list_of_rows
-
-               :param task: (string) name of task:
-               :param priority: (string) priority for task:
-               :param list_of_rows: (list) list of dictionary obj:
-               :return: (list) of dictionary rows, (string) Success
-               """
-        dicRow = {"Task": task, "Priority": priority}
-        list_of_rows.append(dicRow)
-        return list_of_rows, 'Success'
-
-    @staticmethod
-    def remove_data_from_list(task, list_of_rows):
-        """ Removing specified task and priority from list_of_rows and returning new list_of_rows
-
-               :param task: (string) name of task to remove:
-               :param list_of_rows: (list) list of dictionary obj:
-               :return: (list) of dictionary rows, (string) Success
-               """
-        for row in list_of_rows:
-            if row["Task"].lower() == task.lower():
-                list_of_rows.remove(row)
-        return list_of_rows, 'Success'
-
-    @staticmethod
-    def write_data_to_file(file_name, list_of_rows):
-        """ Write list_of_rows data to file
+    def save_data_to_file(file_name, list_of_product_objects):
+        """ Save list_of_product_objects data to file
 
                :param file_name: (string) file to write to:
-               :param list_of_rows: (list) list of dictionary obj:
-               :return: (list) of dictionary rows, (string) Success
+               :param list_of_product_objects: (list) list of product objects:
+               :return: (list) of product objects
                """
         try:
             f = open(file_name, "w")
-            for row in list_of_rows:
-                f.write(str(row["Task"]) + "," + str(row["Priority"] + "\n"))
+            for item in list_of_product_objects:
+                f.write(item.product_name() + "," + item.product_price() + "\n")
             f.close()
         except:
             print('Unable to open/write to source file')
-        return list_of_rows, 'Success'
+        return list_of_product_objects
 
 # Processing  ------------------------------------------------------------- #
 
 # Presentation (Input/Output)  -------------------------------------------- #
 class IO:
-    """ Performs Input and Output tasks """
+    """ Performs Input and Output tasks
 
+    methods:
+        print_menu_options()
+        input_menu_choice() -> (str: menu choice)
+        print_current_products_in_list(list_of_product_objects)
+        input_yes_no_choice(message) -> (str:reply from user)
+        input_press_to_continue(optional_message='')
+        input_new_product_and_price() -> (obj: new product object)
+
+    changelog: (When,Who,What)
+        RRoot,1.1.2030,Created Class
+        Sheri Elgin, Dec 6, 2021, Modified code to complete assignment 8
+    """
     @staticmethod
-    def print_menu_Tasks():
+    def print_menu_options():
         """  Display a menu of choices to the user
 
         :return: nothing
         """
         print('''
         Menu of Options
-        1) Add a new Task
-        2) Remove an existing Task
-        3) Save Data to File        
-        4) Reload Data from File
-        5) Exit Program
+        1) Show current list of products
+        2) Add product to list
+        3) Save data to file and exit program   
+        4) Exit program without save    
         ''')
-        print()  # Add an extra line for looks
+        print()  # Add an extra line
 
     @staticmethod
     def input_menu_choice():
@@ -129,22 +143,22 @@ class IO:
 
         :return: string
         """
-        choice = str(input("Which option would you like to perform? [1 to 5] - ")).strip()
-        print()  # Add an extra line for looks
+        choice = str(input("Which option would you like to perform? [1 to 3] - ")).strip()
+        print()  # Add an extra line
         return choice
 
     @staticmethod
-    def print_current_Tasks_in_list(list_of_rows):
-        """ Shows the current Tasks in the list of dictionaries rows
+    def print_current_products_in_list(list_of_product_objects):
+        """ Shows the current Products in the list
 
-        :param list_of_rows: (list) of rows you want to display
+        :param list_of_product_objects: (list) of rows you want to display
         :return: nothing
         """
-        print("******* The current Tasks ToDo are: *******")
-        for row in list_of_rows:
-            print(row["Task"] + " (" + row["Priority"] + ")")
+        print("******* Current Products: *******")
+        for item in list_of_product_objects:
+            print(item.product_name() + ", " + item.product_price() + "\n")
         print("*******************************************")
-        print()  # Add an extra line for looks
+        print()  # Add an extra line
 
     @staticmethod
     def input_yes_no_choice(message):
@@ -161,29 +175,21 @@ class IO:
         :param optional_message:  An optional message you want to display
         :return: nothing
         """
-        print(optional_message)
+        print(optional_message + '\n')
         input('Press the [Enter] key to continue.')
 
     @staticmethod
-    def input_new_task_and_priority():
-        """ Get task and associated priority from user
+    def input_new_product_and_price():
+        """ Get product and price from user
 
-        :return: dictionary obj
+        :return: Product object
         """
-        task = input("Please enter a task you wish to add: ")
-        priority = input("What priority level (1-5) do you want to assign it? ")
-        return {"Task": task, "Priority": priority}
+        product = input("Please enter product name: ")
+        price = input("Product price: ")
+        price = float(price)
+        objProduct = Product(product, price)
 
-    @staticmethod
-    def input_task_to_remove():
-        """ Get task to remove from user
-
-        :return: string
-        """
-        task = input("Which item do you wish to remove? ")
-        return task
-
-
+        return objProduct
 
 # Presentation (Input/Output)  -------------------------------------------- #
 
@@ -199,49 +205,40 @@ class IO:
 
 
 # Step 1 - When the program starts, Load data from ToDoFile.txt.
-Processor.read_data_from_file(strFileName, lstTable)  # read file data
+FileProcessor.read_data_from_file(strFileName, lstOfProductObjects)  # read file data
 
 # Step 2 - Display a menu of choices to the user
 while(True):
     # Step 3 Show current data
-    IO.print_current_Tasks_in_list(lstTable)  # Show current data in the list/table
-    IO.print_menu_Tasks()  # Shows menu
+    IO.print_current_products_in_list(lstOfProductObjects)  # Show current data from file
+    IO.print_menu_options()  # Shows menu
     strChoice = IO.input_menu_choice()  # Get menu option
     
     # Step 4 - Process user's menu choice
-    if strChoice.strip() == '1':  # Add a new Task
-        dicRow = IO.input_new_task_and_priority()
-        Processor.add_data_to_list(dicRow["Task"], dicRow["Priority"], lstTable)
-        IO.input_press_to_continue(strStatus)
+    if strChoice.strip() == '1':  # Show current list
+        IO.print_current_products_in_list(lstOfProductObjects)
+        IO.input_press_to_continue()
         continue  # to show the menu
 
-    elif strChoice == '2':  # Remove an existing Task
-        strTask = IO.input_task_to_remove()
-        Processor.remove_data_from_list(strTask, lstTable)
-        IO.input_press_to_continue(strStatus)
+    elif strChoice == '2':  # Add a product and price to list
+        objProd = IO.input_new_product_and_price()
+        lstOfProductObjects.append(objProd)
+        IO.input_press_to_continue()
         continue  # to show the menu
 
     elif strChoice == '3':   # Save Data to File
-        strChoice = IO.input_yes_no_choice("Save this data to file? (y/n) - ")
+        strChoice = IO.input_yes_no_choice("Save all product data to file? (y/n) - ")
         if strChoice.lower() == "y":
-            Processor.write_data_to_file(strFileName, lstTable)
-            IO.input_press_to_continue(strStatus)
+            FileProcessor.save_data_to_file(strFileName, lstOfProductObjects)
+            IO.input_press_to_continue()
         else:
             IO.input_press_to_continue("Save Cancelled!")
         continue  # to show the menu
 
-    elif strChoice == '4':  # Reload Data from File
-        print("Warning: Unsaved Data Will Be Lost!")
-        strChoice = IO.input_yes_no_choice("Are you sure you want to reload data from file? (y/n) -  ")
-        if strChoice.lower() == 'y':
-            Processor.read_data_from_file(strFileName, lstTable)  # read file data
-            IO.input_press_to_continue(strStatus)
-        else:
-            IO.input_press_to_continue("File Reload  Cancelled!")
+    elif strChoice != '4':  # Invalid menu option
+        print("Warning: Invalid menu option selected, please only enter 1, 2, 3 or 4.")
         continue  # to show the menu
 
-    elif strChoice == '5':  #  Exit Program
+    else:  #  Exit Program
         print("Goodbye!")
         break   # and Exit
-
-
